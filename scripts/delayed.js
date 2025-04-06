7650d4e607e0d5cc3330ef6b51ab162fa1473fed
Sean McAuliffe <sean.mcauliffe@herodigital.com>
2025-04-06 14:35:56 -0400

    footer style fix

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,6 +4,6 @@ const spotify = document.createElement('div');
 spotify.classList.add('spotify-embed');
 spotify.innerHTML = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/1FEtZvz9cTqsN4LofCmwYm?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
 
-const footerDiv = document.querySelector('.footer');
+const footerDiv = document.querySelector('.footer > div');
 footerDiv.prepend(spotify);
 spotify.parentNode.classList.add('section');
