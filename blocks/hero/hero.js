f6d50ab558ceae3e5aab999ab7550aa3b76d0e8d
Sean McAuliffe <sean.mcauliffe@herodigital.com>
2025-04-05 22:04:08 -0400

    some linting

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,6 +1,4 @@
-import { div } from '../../scripts/dom-helpers.js';
-
-export default function decorate (block) {
+export default function decorate(block) {
   const rows = [...block.children];
   const [mediaRow, contentRow] = rows;
   mediaRow.classList.add('hero-media');
