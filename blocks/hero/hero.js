93e007b6795c718ac5c44b39e4f4166860f8728a
Sean McAuliffe <smcauliffe2@gmail.com>
2025-04-05 22:06:09 -0400

    Open props (#1)
    * testing open-props
    
    * local normalize file
    
    * messing around with layout and open props lib
    
    * pretty decent hero layout
    
    * tweakin
    
    * refactor: update CSS variables and layout for header and hero sections
    
    * obsessivly kerning
    
    * stuff
    
    * some linting
    
    ---------
    
    Co-authored-by: Sean McAuliffe <sean.mcauliffe@herodigital.com>

---
+++ blocks/hero/hero.js
@@ -0,0 +1,12 @@
+export default function decorate(block) {
+  const rows = [...block.children];
+  const [mediaRow, contentRow] = rows;
+  mediaRow.classList.add('hero-media');
+  contentRow.classList.add('hero-content');
+
+  // unwrap the media and content (removes extra div)
+  const mediaWrap = mediaRow.querySelector(':scope > div');
+  const contentWrap = contentRow.querySelector(':scope > div');
+  mediaWrap.replaceWith(...mediaWrap.childNodes);
+  contentWrap.replaceWith(...contentWrap.childNodes);
+}
