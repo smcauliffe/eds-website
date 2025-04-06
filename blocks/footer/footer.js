95a4a6c81c38ebd8fb80ebf87b79a1274a4b2691
Sean McAuliffe <sean.mcauliffe@herodigital.com>
2025-04-06 14:29:53 -0400

    move spotify load to delayed.js

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -16,11 +16,5 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
-  const spotify = document.createElement('div');
-  spotify.classList.add('spotify-embed');
-  spotify.innerHTML = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/1FEtZvz9cTqsN4LofCmwYm?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
-
-  footer.prepend(spotify);
-  spotify.parentNode.classList.add('section');
   block.append(footer);
 }
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,9 @@
 // add delayed functionality here
+
+const spotify = document.createElement('div');
+spotify.classList.add('spotify-embed');
+spotify.innerHTML = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/1FEtZvz9cTqsN4LofCmwYm?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
+
+const footerDiv = document.querySelector('.footer');
+footerDiv.prepend(spotify);
+spotify.parentNode.classList.add('section');
