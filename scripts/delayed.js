723a126928b8b66edd59cf3ae7e9de971b400397
Sean McAuliffe <sean.mcauliffe@herodigital.com>
2025-04-07 15:07:10 -0400

    make linter happy

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -2,7 +2,7 @@
 
 const spotify = document.createElement('div');
 spotify.classList.add('spotify-embed');
-spotify.innerHTML = `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/1FEtZvz9cTqsN4LofCmwYm?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
+spotify.innerHTML = '<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/1FEtZvz9cTqsN4LofCmwYm?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>';
 
 const footerDiv = document.querySelector('.footer > div');
 footerDiv.prepend(spotify);
